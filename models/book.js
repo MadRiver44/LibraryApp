4d3294f89650bc6e1d87d2672c4b41b08842bc6a
Kevin Turney <super996@optonline.net>
2017-12-12 12:33:22 -0500

    added new

--- models/book.js
+++ models/book.js
@@ -1,20 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var BookSchema = Schema({
-  title: {type: String, required: true},
+var BookSchema = new Schema({
+  title: { type: String, required: true },
   // author is a reference to a singl Author model object
-  author: {type: Schema.ObjectId, ref: 'Author', required: true},
-  summary: {type: String, required: true},
-  isbn: {type: String, required: true},
+  author: { type: Schema.ObjectId, ref: 'Author', required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
   //genre is a reference to an array of Genre model objects
-  genre: [{type: Schema.ObjectId, ref: 'Genre'}]
+  genre: [{ type: Schema.ObjectId, ref: 'Genre' }],
 });
 
 // Virtual for books URL
-BookSchema.virtual('url')
-  .get(function() {
-    return '/catalog/book/' + this._id;
-  });
+BookSchema.virtual('url').get(function() {
+  return '/catalog/book/' + this._id;
+});
 
 module.exports = mongoose.model('Book', BookSchema);
