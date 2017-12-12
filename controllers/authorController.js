b361a565b249119866eebd5aedd02189a17944f7
Kevin Turney <super996@optonline.net>
2017-12-12 11:44:17 -0500

    fix minor errors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -66,11 +66,11 @@ exports.author_create_post = function(req, res, next) {
   req
     .checkBody('date_of_birth', 'Invalid date')
     .optional({ checkFalsy: true })
-    .isISO8061();
+    .isDate();
   req
     .checkBody('date_of_death', 'Invalid date')
     .optional({ checkFalsy: true })
-    .isISO8061();
+    .isDate();
 
   req.sanitize('first_name').escape();
   req.sanitize('family_name').escape();
@@ -197,11 +197,11 @@ exports.author_update_post = function(req, res, next) {
   req
     .checkBody('date_of_birth', 'Invalid date')
     .optional({ checkFalsy: true })
-    .isISO8061();
+    .isDate();
   req
     .checkBody('date_of_death', 'Invalid date')
     .optional({ checkFalsy: true })
-    .isISO8061();
+    .isDate();
   req.sanitize('first_name').escape();
   req.sanitize('family_name').escape();
   req.sanitize('id').escape();
